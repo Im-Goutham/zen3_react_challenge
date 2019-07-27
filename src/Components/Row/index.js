e8a1e48b4216d958ab7e3fb505e25b106a662bc3
Goutham <goutham.moka@fptechscience.com>
2019-07-28 05:25:46 +0530

    Changes

--- src/Components/Row/index.js
+++ src/Components/Row/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import './index.css';
-import { Motion, spring } from 'react-motion';
+
 
 function RowComponent(props){
-    return (
-        <Motion
-      defaultStyle={{ scale: 0.5 }}
-      style={{ scale: spring(1, { stiffness: 60, damping: 10 }) }}
-    >
-      {interpolatedStyle =>  <div scale={interpolatedStyle.scale} className="row" {...props}>
+    return ( <div className="row" {...props}>
              {props.children}
-        </div>}
-    </Motion>
-       
+        </div>
     );
   }
 
